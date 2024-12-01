43f8b2110aa9983137f448e9e440f0889a19e174
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-01 21:13:09 +0530

    Category in now connected with h2 database

--- src/main/java/com/ecommerce/project/services/CategoryServiceImp.java
+++ src/main/java/com/ecommerce/project/services/CategoryServiceImp.java
@@ -1,8 +1,9 @@
 package com.ecommerce.project.services;
 
 import com.ecommerce.project.models.Category;
+import com.ecommerce.project.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -10,28 +11,31 @@ import java.util.*;
 
 @Service
 public class CategoryServiceImp implements CategoryService {
-    private List<Category> categoryList = new ArrayList<>();
+    //private List<Category> categoryList = new ArrayList<>();
     private long id = 1;
+    @Autowired
+    CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getCategoryList() {
-        return categoryList;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
         category.setCategoryId(id++);
-        categoryList.add(category);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(long id) {
+        List<Category> categoryList = categoryRepository.findAll();
         Category category = categoryList.stream()
                 .filter(c -> c.getCategoryId() == id)
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category with CategoryId: " + id + " not Found"));
 
-        categoryList.remove(category);
+        categoryRepository.delete(category);
         return "Category with CategoryId: " + id + " is Deleted Successfully";
 
 
@@ -39,13 +43,18 @@ public class CategoryServiceImp implements CategoryService {
 
     @Override
     public String updateCategory(Category category, long id) {
-        Category updateCategory = categoryList.stream()
-                .filter(c -> c.getCategoryId() == id)
-                .findFirst()
+        // Find the category to update
+        Category existingCategory = categoryRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category with CategoryId: " + id + " not Found"));
-        updateCategory.setCategoryName(category.getCategoryName());
+
+        // Update the category's fields
+        existingCategory.setCategoryName(category.getCategoryName());
+
+
+        // Save the updated category to the repository
+        categoryRepository.save(existingCategory);
 
         return "Category with CategoryId: " + id + " is Updated Successfully";
-
     }
+
 }
