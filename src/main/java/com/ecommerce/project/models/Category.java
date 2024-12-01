9779b9b84cc8e817efc2ebe84cf5a898b7d43f4c
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-01 22:02:50 +0530

    lombok configured

--- src/main/java/com/ecommerce/project/models/Category.java
+++ src/main/java/com/ecommerce/project/models/Category.java
@@ -4,7 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Category {
     @Id
@@ -12,27 +18,5 @@ public class Category {
     private Long categoryId;
     private String categoryName;
 
-    public Category() {
-    }
 
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
