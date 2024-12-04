5fde45a5ca1f907625925513aa689813d2e598c0
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-04 23:42:57 +0530

    Default sorting details added

--- src/main/java/com/ecommerce/project/config/AppConstants.java
+++ src/main/java/com/ecommerce/project/config/AppConstants.java
@@ -3,4 +3,8 @@ package com.ecommerce.project.config;
 public class AppConstants {
     public static final String DEFAULT_PAGE_NUMBER = "0";
     public static final String DEFAULT_PAGE_SIZE = "50";
+    public static final String DEFAULT_SORT_BY = "categoryId";
+    public static final String DEFAULT_SORT_ORDER = "asc";
+
+
 }
