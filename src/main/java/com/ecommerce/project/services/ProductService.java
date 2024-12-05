9c3f7ec7e894a862b17bca27158a946ec61e7bed
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-06 00:09:17 +0530

    Add product endpoint created

--- src/main/java/com/ecommerce/project/services/ProductService.java
+++ src/main/java/com/ecommerce/project/services/ProductService.java
@@ -1,4 +1,8 @@
 package com.ecommerce.project.services;
 
+import com.ecommerce.project.models.Product;
+import com.ecommerce.project.payload.ProductDTO;
+
 public interface ProductService {
+    ProductDTO addProduct(Long categoryId, Product product);
 }
--- src/main/java/com/ecommerce/project/services/ProductServiceImp.java
+++ src/main/java/com/ecommerce/project/services/ProductServiceImp.java
@@ -1,7 +1,34 @@
 package com.ecommerce.project.services;
 
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
+import com.ecommerce.project.models.Category;
+import com.ecommerce.project.models.Product;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.repositories.CategoryRepository;
+import com.ecommerce.project.repositories.ProductRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProductServiceImp implements ProductService{
+public class ProductServiceImp implements ProductService {
+    @Autowired
+    ProductRepository productRepository;
+    @Autowired
+    CategoryRepository categoryRepository;
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Override
+    public ProductDTO addProduct(Long categoryId, Product product) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(()-> new ResourceNotFoundException("Category","categoryId",categoryId));
+
+        product.setImage("default.png");
+        product.setCategory(category);
+        double specialPrice = product.getPrice() - ((product.getDiscount()*0.01)*product.getPrice());
+        product.setSpecialPrice(specialPrice);
+        Product savedProduct=productRepository.save(product);
+        return modelMapper.map(savedProduct, ProductDTO.class);
+    }
 }
