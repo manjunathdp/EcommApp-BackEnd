a40529d3fbf7e7e529a5bb05980a101842a5f4db
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-05 21:51:45 +0530

    @Repository annotation added

--- src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
@@ -4,7 +4,9 @@ import com.ecommerce.project.models.Category;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category,Long> {
 
     Category findByCategoryName(@NotBlank @Size(min = 3,message = "Category name must contain at least 3 characters") String categoryName);
