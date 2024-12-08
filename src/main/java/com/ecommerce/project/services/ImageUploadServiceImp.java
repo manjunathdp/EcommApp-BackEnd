19f8b20752b62ecb2b8a73fb0bf9e85756f953ac
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-08 19:51:13 +0530

    Image upload service upload image Exception handled

--- src/main/java/com/ecommerce/project/services/ImageUploadServiceImp.java
+++ src/main/java/com/ecommerce/project/services/ImageUploadServiceImp.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.services;
 
+import com.ecommerce.project.exceptions.APIException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,13 +18,22 @@ public class ImageUploadServiceImp implements ImageUploadService{
         String originalFileName = image.getOriginalFilename();
         String randomId = UUID.randomUUID().toString();
         //assert originalFileName != null;
-        String imageName = randomId.concat(originalFileName.substring(originalFileName.lastIndexOf('.')));
-        String imagePath = path + File.separator + imageName;
-        File folder = new File(path);
-        if (!folder.exists()) {
-            folder.mkdir();
+//        String imageName = randomId.concat(originalFileName.substring(originalFileName.lastIndexOf('.')));
+        if (originalFileName != null && originalFileName.contains(".") && randomId != null) {
+            String extension = originalFileName.substring(originalFileName.lastIndexOf('.'));
+            String imageName = randomId + extension;
+            String imagePath = path + File.separator + imageName;
+            File folder = new File(path);
+            if (!folder.exists()) {
+                folder.mkdir();
+            }
+            Files.copy(image.getInputStream(), Paths.get(imagePath));
+            return imageName;
+
+        } else {
+            throw new APIException("Invalid input: originalFileName or randomId is null, or originalFileName has no extension.");
         }
-        Files.copy(image.getInputStream(), Paths.get(imagePath));
-        return imageName;
+
+
     }
 }
