381a89b5f7ccf9f606284c775c9f53691dabab59
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-08 23:18:56 +0530

    Validation check added to Product name and product description

--- src/main/java/com/ecommerce/project/controllers/ProductController.java
+++ src/main/java/com/ecommerce/project/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.ecommerce.project.controllers;
 import com.ecommerce.project.payload.ProductDTO;
 import com.ecommerce.project.payload.ProductResponse;
 import com.ecommerce.project.services.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class ProductController {
     ProductService productService;
 
     @PostMapping("/admin/categories/{categoryId}/product")
-    public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductDTO productDTO,
+    public ResponseEntity<ProductDTO> addProduct(@Valid @RequestBody ProductDTO productDTO,
                                                  @PathVariable Long categoryId) {
         ProductDTO savedProductDTO = productService.addProduct(categoryId, productDTO);
         return new ResponseEntity<>(savedProductDTO, HttpStatus.CREATED);
