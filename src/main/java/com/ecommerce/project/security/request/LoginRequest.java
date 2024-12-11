b10001647da1d630f7e5cc0690c1e04d5b219865
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-11 21:46:10 +0530

    signin created

--- src/main/java/com/ecommerce/project/security/request/LoginRequest.java
+++ src/main/java/com/ecommerce/project/security/request/LoginRequest.java
@@ -1,23 +1,14 @@
 package com.ecommerce.project.security.request;
 
-public class LoginRequest {
-    private String username;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String username;
+    @NotBlank
     private String password;
 
-    public String getUsername() {
-        return username;
-    }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
