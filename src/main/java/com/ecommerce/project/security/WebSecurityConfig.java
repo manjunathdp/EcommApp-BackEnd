d8f8dabeb17580b6542a6b1506117dc17c31d772
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-11 23:39:22 +0530

    sign through cookies implemented

--- src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
+++ src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import com.ecommerce.project.models.Role;
 import com.ecommerce.project.models.User;
 import com.ecommerce.project.repositories.RoleRepository;
 import com.ecommerce.project.repositories.UserRepository;
+import com.ecommerce.project.security.jwt.AuthTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -13,11 +14,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 //import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -27,7 +26,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.ecommerce.project.security.jwt.AuthEntryPointJwt;
-import com.ecommerce.project.security.jwt.AuthTokenFilter;
 import com.ecommerce.project.security.services.UserDetailsServiceImpl;
 
 import java.util.Set;
--- src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
+++ src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
@@ -1,14 +1,19 @@
 package com.ecommerce.project.security.response;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+
 
 import java.util.List;
 
 @Data
+
 public class UserInfoResponse {
 
     private Long id;
+
+    @JsonIgnore
     private String jwtToken;
 
     private String username;
@@ -19,6 +24,17 @@ public class UserInfoResponse {
         this.username = username;
         this.roles = roles;
         this.jwtToken = jwtToken;
+    }
+
+    public UserInfoResponse(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public UserInfoResponse(Long id, String username, List<String> roles) {
+        this.id = id;
+        this.username = username;
+        this.roles = roles;
     }
 }
 
