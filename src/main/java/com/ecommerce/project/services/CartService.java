573fc68b554c16b08ce573db34df2e86ba81779d
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-13 17:23:33 +0530

    get all carts endpoint created

--- src/main/java/com/ecommerce/project/services/CartService.java
+++ src/main/java/com/ecommerce/project/services/CartService.java
@@ -2,6 +2,10 @@ package com.ecommerce.project.services;
 
 import com.ecommerce.project.payload.CartDTO;
 
+import java.util.List;
+
 public interface CartService {
     CartDTO addProductToCart(Long productId, Integer quantity);
+
+    List<CartDTO> getAllCarts();
 }
