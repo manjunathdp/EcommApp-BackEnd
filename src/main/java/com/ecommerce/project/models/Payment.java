70532dec99e038279ec0d7810caf9077ce7e3aaf
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-20 22:43:30 +0530

    constructor updated

--- src/main/java/com/ecommerce/project/models/Payment.java
+++ src/main/java/com/ecommerce/project/models/Payment.java
@@ -18,21 +18,23 @@ public class Payment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long paymentId;
 
-    @OneToOne(mappedBy = "payment", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @OneToOne(mappedBy = "payment", cascade = { CascadeType.PERSIST, CascadeType.MERGE })
     private Order order;
 
     @NotBlank
     @Size(min = 4, message = "Payment method must contain at least 4 characters")
     private String paymentMethod;
-
+	
     private String pgPaymentId;
     private String pgStatus;
     private String pgResponseMessage;
 
     private String pgName;
 
-    public Payment(Long paymentId, String pgPaymentId, String pgStatus, String pgResponseMessage, String pgName) {
-        this.paymentId = paymentId;
+
+    public Payment(String paymentMethod, String pgPaymentId, String pgStatus,
+                   String pgResponseMessage, String pgName) {
+        this.paymentMethod = paymentMethod;
         this.pgPaymentId = pgPaymentId;
         this.pgStatus = pgStatus;
         this.pgResponseMessage = pgResponseMessage;
